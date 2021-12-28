cd378cab7b261f0193a78da2eb9909ca11b32bbd
ante <ante.kotarac@gmail.com>
2021-12-29 00:09:36 +0100

    more setup

--- game_loop.py
+++ game_loop.py
@@ -1,13 +1,15 @@
 import pygame
 
-def run_loop_state():
+def run_loop_state(display):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            return False         
 
     return True
 
 def run_game_loop(display):
     run_loop = True
     while run_loop:
-        run_loop = run_loop_state()
+        run_loop = run_loop_state(display)
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 import pygame
 
+from screen import Screen
+
 class Setup:
     @staticmethod
     def create():
         pygame.init()
-        return pygame.display.set_mode((800, 600))
+        return pygame.display.set_mode((Screen.WIDTH, Screen.HEIGHT))
 
     @staticmethod
     def destroy():
