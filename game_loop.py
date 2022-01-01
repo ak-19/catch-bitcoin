044fc1f930e7f7ad3258d22b419bb73c8b18fa67
ante <ante.kotarac@gmail.com>
2022-01-02 00:46:35 +0100

    basci game play

--- game_loop.py
+++ game_loop.py
@@ -21,9 +21,11 @@ class GameLoop:
         self.clock = pygame.time.Clock()
         self.FPS = 60
         self.font = pygame.font.Font('assets/BlackgroundsRegular.ttf', 32)
+        self.endgame_font = pygame.font.Font('assets/BlackgroundsRegular.ttf', 64)
 
         self.miss_sound = pygame.mixer.Sound('assets/miss.wav')
         self.pickup_sound = pygame.mixer.Sound('assets/pickup.wav')
+        self.loss_sound = pygame.mixer.Sound('assets/loss.wav')
         pygame.mixer.music.load('assets/draft-monk-ambience.mp3')
         pygame.mixer.music.play(-1)
 
@@ -34,11 +36,18 @@ class GameLoop:
         self.coin = Bitcoin(Screen.WIDTH - 32, randint(60, Screen.HEIGHT - 32))
 
     def run_loop_state(self):
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 return False              
+
+        if self.lost():   
+            self.loss_sound.play(0)         
+            pygame.mixer.music.stop()
+            self.clock.tick(self.FPS)
+            return True
 
         pressed = pygame.key.get_pressed()
 
@@ -47,17 +56,19 @@ class GameLoop:
         elif pressed[pygame.K_DOWN] and self.man.rect.y < Screen.HEIGHT - 64:
             self.man.rect.y += self.player_velocity
 
-        if self.coin.rect.x - Constants.COIN_START_VELOCITY < 0:     
+        if self.coin.rect.x - self.coin_velocity < 0:     
             self.lives -= 1       
             self.make_new_coin()
             self.miss_sound.play()        
         else:
-            self.coin.rect.x -= Constants.COIN_START_VELOCITY
+            self.coin.rect.x -= self.coin_velocity
 
         if self.man.rect.colliderect(self.coin.rect):
             self.score += 1
             self.make_new_coin()
             self.pickup_sound.play()
+            self.coin_velocity += Constants.COIN_ACCELERATION
+
 
         self.draw_objects()
 
@@ -65,12 +76,23 @@ class GameLoop:
 
         return True
 
-    def draw_objects(self):
-        self.draw_header()
+    def lost(self):
+        if self.lives > 0:
+            return False
 
+        
+        end = self.endgame_font.render('Game over', False, Colors.GREEN, Colors.DARKGREEN)
+        end_rect = end.get_rect()
+        end_rect.center = (Screen.WIDTH // 2, Screen.HEIGHT // 2)        
+        self.display.blit(end, end_rect)
+        pygame.display.update()
+        return True
+
+    def draw_objects(self):
+        self.draw()
         pygame.display.update()
 
-    def draw_header(self):
+    def draw(self):
         score_text = self.font.render(f'Score: {self.score}', False, Colors.GREEN, Colors.BLACK)
         score_text_rect = score_text.get_rect()
         score_text_rect.topleft = (20, 20)
